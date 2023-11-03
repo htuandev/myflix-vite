c45804d5685d43c9099e721c7098b5d65f3fc8bf
htuandev <130202652+htuandev@users.noreply.github.com>
2023-11-04 00:35:03 +0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section className='flex min-h-screen items-center justify-center'>
+      <div className='rounded-full bg-[#ededed] p-8'>
+        <img src='/myflix.svg' width={120} height={120} alt='Logo' />
+      </div>
+    </section>
+  );
+}
+
+export default App;
