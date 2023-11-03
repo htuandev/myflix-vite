1c9d830928829bb631f5b439aca599bf6a885ba1
htuandev <130202652+htuandev@users.noreply.github.com>
2023-11-04 00:55:37 +0700

    Antd Config

---
+++ src/constants/themConfig.ts
@@ -0,0 +1,33 @@
+import { ThemeConfig, theme } from 'antd';
+import colors from './colors';
+
+const themeConfig: ThemeConfig = {
+  algorithm: theme.darkAlgorithm,
+  token: {
+    fontSize: 16,
+    fontFamily: '"Roboto", sans-serif',
+    lineWidth: 2,
+    controlHeight: 40,
+    controlOutline: 'none',
+    screenSM: 640,
+    screenSMMax: 767,
+    screenSMMin: 640,
+    screenMD: 768,
+    screenMDMax: 1023,
+    screenMDMin: 768,
+    screenLG: 1024,
+    screenLGMax: 1279,
+    screenLGMin: 1024,
+    screenXL: 1280,
+    screenXLMax: 1535,
+    screenXLMin: 1280,
+    screenXXL: 1536,
+    screenXXLMin: 1536,
+    colorTextBase: colors.dark[200],
+    colorBgBase: colors.body,
+    colorBgContainer: colors.dark[950],
+    colorBgElevated: colors.dark[925]
+  }
+};
+
+export default themeConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConfigProvider, Empty } from 'antd';
 import App from './App.tsx';
 import './assets/styles/index.css';
+import themeConfig from './constants/themConfig.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ConfigProvider
+    theme={themeConfig}
+    renderEmpty={() => <Empty description={false} className=' d-center min-h-[50vh]' />}
+  >
     <App />
-  </React.StrictMode>
+  </ConfigProvider>
 );
