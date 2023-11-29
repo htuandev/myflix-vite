ba389092d74de03636acf6301721c96e97fd1ad2
htuandev <130202652+htuandev@users.noreply.github.com>
2023-11-29 11:15:39 +0700

    Update personApi.ts

--- src/api/personApi.ts
+++ src/api/personApi.ts
@@ -1,5 +1,5 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
-import { Prettify, DataList, SearchParams, SuccessResponse, Person } from '@/types';
+import { Prettify, DataList, SearchParams, SuccessResponse, IPerson } from '@/types';
 import { baseQuery, updateSearchParams } from '@/utils';
 
 type Response<T> = Prettify<SuccessResponse & { data: T }>;
@@ -9,12 +9,12 @@ export const personApi = createApi({
   baseQuery: baseQuery('person'),
   tagTypes: ['People', 'Person'],
   endpoints: (build) => ({
-    getPeople: build.query<DataList<Person>, SearchParams>({
+    getPeople: build.query<DataList<IPerson>, SearchParams>({
       query: (params) => ({
         url: '',
         params: updateSearchParams(params)
       }),
-      transformResponse: (res: Response<DataList<Person>>) => res.data,
+      transformResponse: (res: Response<DataList<IPerson>>) => res.data,
       providesTags: (result) => {
         if (result) {
           const final = [
@@ -26,12 +26,12 @@ export const personApi = createApi({
         return [{ type: 'People' as const, id: 'LIST' }];
       }
     }),
-    getPersonById: build.query<Person, string>({
+    getPersonById: build.query<IPerson, string>({
       query: (id) => id,
-      transformResponse: (res: Response<Person>) => res.data,
+      transformResponse: (res: Response<IPerson>) => res.data,
       providesTags: (result) => (result ? [{ type: 'Person', id: result._id }] : [])
     }),
-    addPerson: build.mutation<SuccessResponse, Person>({
+    addPerson: build.mutation<SuccessResponse, IPerson>({
       query: (body) => ({
         url: '',
         method: 'POST',
@@ -39,7 +39,7 @@ export const personApi = createApi({
       }),
       invalidatesTags: (result) => (result ? [{ type: 'People', id: 'LIST' }] : [])
     }),
-    updatePerson: build.mutation<Response<Person>, Person>({
+    updatePerson: build.mutation<Response<IPerson>, IPerson>({
       query: (body) => ({
         url: '',
         method: 'PUT',
